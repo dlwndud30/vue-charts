64a42f5f2b2740fb3177e0667d61620bfc54fd64
wnduddl30 <leejy3022@naver.com>
2023-01-12 23:01:49 +0900

    BarChart

---
+++ chart-lib/src/components/BarChart.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <canvas id="myChart" width="400" height="400"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from 'chart.js/auto'
+
+export default {
+mounted(){
+    (async function() {
+      const data = [
+        { year: 2010, count: 10 },
+        { year: 2011, count: 20 },
+        { year: 2012, count: 15 },
+        { year: 2013, count: 25 },
+        { year: 2014, count: 22 },
+        { year: 2015, count: 30 },
+        { year: 2016, count: 28 },
+      ];
+
+      new Chart(
+        document.getElementById('myChart'),
+        {
+          type: 'bar',
+          data: {
+            labels: data.map(row => row.year),
+            datasets: [
+              {
+                label: 'Acquisitions by year',
+                data: data.map(row => row.count)
+              }
+            ]
+          }
+        }
+      );
+    })();
+  }
+}
+</script>
+
+<style>
+
+</style>
