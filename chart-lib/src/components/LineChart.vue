049d48ca21dc1914212a3bd64981cca7a94e42a9
wnduddl30 <leejy3022@naver.com>
2023-01-12 23:11:12 +0900

    LineChart

---
+++ chart-lib/src/components/LineChart.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <canvas id="lineChart"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from 'chart.js/auto';
+export default {
+    mounted(){
+        const ctx = document.getElementById('lineChart');
+
+        new Chart(ctx, {
+            type: 'line',
+            data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+            }]
+            },
+            options: {
+            scales: {
+                y: {
+                beginAtZero: true
+                }
+            }
+            }
+        });
+    }
+}
+</script>
+
+<style>
+
+</style>
