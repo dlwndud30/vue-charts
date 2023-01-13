00eb649dd085d0b5c3e3852934e958d36080dbae
wnduddl30 <leejy3022@naver.com>
2023-01-14 00:34:09 +0900

    controlled design patterns

---
+++ design/controlled/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <check-box v-model="checked"></check-box>
+  </div>
+</template>
+
+<script>
+import CheckBox from './components/CheckBox.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    CheckBox
+  },
+  data(){
+    return {
+      checked: false,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ design/controlled/src/components/CheckBox.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input type="checkbox" :value="value" @click="toggle">
+  </div>
+</template>
+
+<script>
+export default {
+  props:['value'],
+  methods:{
+    toggle(){
+        this.$emit('input', !this.value);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
