b56b7832850bc9eaa354ff88c3ad807506e46cec
wnduddl30 <leejy3022@naver.com>
2023-01-13 22:05:09 +0900

    slot design pattern

---
+++ design/slots/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <ul>
+      <item-element v-for="(item, idx) in items" :key="idx">{{ item }}</item-element>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ItemElement from '@/ItemElement.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ItemElement,
+  },
+  data(){
+    return{
+      items:['아이템1', '아이템2', '아이템3', '아이템4', '아이템5'],
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
