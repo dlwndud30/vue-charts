532110e72f2a5fb93cb9d94b88e16400e769d61e
wnduddl30 <leejy3022@naver.com>
2023-01-14 22:57:41 +0900

    renderless Component

---
+++ design/renderless/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <fetch-data url="https://jsonplaceholder.typicode.com/users/1">
+      <div slot-scope="{response, loading}">
+        <div v-if="!loading">
+          {{ response }}
+        </div>
+        <div v-else>
+          Loading..
+        </div>
+      </div>
+    </fetch-data>
+  </div>
+</template>
+
+<script>
+import FetchData from './components/FetchData.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FetchData
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
